502aebd682ee111183090b793f2ad49819351b7e
ashwinicet1 <ashwinicet1@gmail.com>
2025-06-22 11:02:57 +0530

    Add files via upload

---
+++ src/test/java/pack10am/MouseMovements_22jun.java
@@ -0,0 +1,60 @@
+package pack10am;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+
+
+public class MouseMovements_22jun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		
+		driver.get("https://demo.nopcommerce.com/");
+		
+		
+		 WebElement computerwebelement=driver.findElement(By.xpath("//a[text()='Computers ']"));
+		
+		 WebElement notebookswebelement=driver.findElement(By.xpath("//a[text()='Notebooks ']"));
+			
+		 Actions mouse1=new Actions(driver);
+		 
+		 mouse1.moveToElement(computerwebelement).moveToElement(notebookswebelement).click().perform();
+		 
+		 
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//driver.close();
+		driver.quit();
+		
+		//   System.out.println(
+		
+	}
+
+}
