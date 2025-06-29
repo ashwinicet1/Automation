6847d3ee97b696e41f3d078d30984a3c9f38515c
ashwinicet1 <ashwinicet1@gmail.com>
2025-06-29 10:52:20 +0530

    Add files via upload

---
+++ src/test/java/pack10am/Draganddrop_29jun.java
@@ -0,0 +1,60 @@
+package pack10am;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+
+
+public class Draganddrop_29jun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html#google_vignette");
+		
+		
+	WebElement	oslo=driver.findElement(By.id("box1"));
+		
+	WebElement	norway=driver.findElement(By.id("box101"));
+	
+	Actions drag=new Actions(driver);
+	
+	drag.dragAndDrop(oslo, norway).perform();
+	
+	
+	
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//driver.close();
+		driver.quit();
+		
+		//   System.out.println(
+		
+	}
+
+}
