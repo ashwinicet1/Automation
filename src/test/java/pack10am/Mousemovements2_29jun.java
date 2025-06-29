1cc1187fbec710563aa6581086dbc45381f77ea8
ashwinicet1 <ashwinicet1@gmail.com>
2025-06-29 10:55:15 +0530

    Add files via upload

---
+++ src/test/java/pack10am/Mousemovements2_29jun.java
@@ -0,0 +1,55 @@
+package pack10am;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+
+
+public class Mousemovements2_29jun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+		
+	WebElement	rightclick=driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+		
+		
+	Actions rightclickaction=new Actions(driver);
+	
+	rightclickaction.contextClick(rightclick).perform();
+	
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//driver.close();
+		driver.quit();
+		
+		//   System.out.println(
+		
+	}
+
+}
