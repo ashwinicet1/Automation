ba7bcacb4822ceb5a23a52922dc91f8b09f3c577
ashwinicet1 <ashwinicet1@gmail.com>
2025-06-29 10:59:05 +0530

    Add files via upload

---
+++ src/test/java/pack10am/Doubleclick_29jun.java
@@ -0,0 +1,63 @@
+package pack10am;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+
+
+public class Doubleclick_29jun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3");
+		
+		driver.switchTo().frame("iframeResult");
+		
+		
+		WebElement doubleclickweb=driver.findElement(By.xpath("//button[text()='Copy Text']"));
+		
+		
+		Actions doubleclickaction=new Actions(driver);
+		
+		
+		doubleclickaction.doubleClick(doubleclickweb).perform();
+		
+		
+		
+		
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//driver.close();
+		driver.quit();
+		
+		//   System.out.println(
+		
+	}
+
+}
